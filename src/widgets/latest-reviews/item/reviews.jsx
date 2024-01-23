3265753b8ce84d8f11c172f45253a89636368e92
ynherb <ynherb299@gmail.com>
2024-01-23 19:29:37 +0200

    fixed svg

--- src/widgets/latest-reviews/item/reviews.jsx
+++ src/widgets/latest-reviews/item/reviews.jsx
@@ -1,10 +1,4 @@
-import Image from 'next/image';
-import StarEmpty from '@/assets/images/svg/star-empty.svg';
-import StarHalf from '@/assets/images/svg/star-half.svg';
-import StarFull from '@/assets/images/svg/star-full.svg';
 import './style.css';
-
-const STAR_WIDTH = 17;
 export default function Reviews({rating, count_reviews, text_review = true, number_review = true }) {
 
     const stars = Array.from(new Array(5)).map((_, index) => index)
@@ -20,19 +14,19 @@ export default function Reviews({rating, count_reviews, text_review = true, numb
                 <div>
                     {!!rating && stars.map(value => {
                         if((rating - value) >= 1)
-                            return <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
+                            return <svg key={value} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
                                         fill="#F6C43D">
                                 <path
                                     d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
                             </svg>;
                         else if ((rating - value) > 0)
-                            return <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 536 512"
+                            return <svg key={value} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 536 512"
                                         fill="#F6C43D">
                                 <path
                                     d="M508.55 171.51L362.18 150.2 296.77 17.81C290.89 5.98 279.42 0 267.95 0c-11.4 0-22.79 5.9-28.69 17.81l-65.43 132.38-146.38 21.29c-26.25 3.8-36.77 36.09-17.74 54.59l105.89 103-25.06 145.48C86.98 495.33 103.57 512 122.15 512c4.93 0 10-1.17 14.87-3.75l130.95-68.68 130.94 68.7c4.86 2.55 9.92 3.71 14.83 3.71 18.6 0 35.22-16.61 31.66-37.4l-25.03-145.49 105.91-102.98c19.04-18.5 8.52-50.8-17.73-54.6zm-121.74 123.2l-18.12 17.62 4.28 24.88 19.52 113.45-102.13-53.59-22.38-11.74.03-317.19 51.03 103.29 11.18 22.63 25.01 3.64 114.23 16.63-82.65 80.38z"/>
                             </svg>;
                         else
-                            return <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
+                            return <svg key={value} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
                                         fill="#F6C43D">
                                 <path
                                     d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/>
