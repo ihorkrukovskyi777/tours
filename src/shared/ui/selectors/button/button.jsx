0c9e6bf91a3e5093fcb1ac6c8ff9012988f819f7
ynherb <ynherb299@gmail.com>
2024-01-23 20:01:36 +0200

    fixed svg

--- src/shared/ui/selectors/button/button.jsx
+++ src/shared/ui/selectors/button/button.jsx
@@ -1,9 +1,18 @@
+'use client';
 import Image from 'next/image';
 import './style.css';
 
-export default function Button({children , icon = null ,customClass='' }) {
+export default function Button({children , icon = null , onClick= () => {} , prevent=false , customClass='' }) {
     return (
-        <button className={`button_custom ${customClass}`} >
+        <button className={`button_custom ${customClass}`}
+            onClick={ (e) => {
+                if(prevent) {
+                    e.preventDefault();
+                }
+                    onClick();
+                }
+            }
+        >
             {children}
             {icon ? <Image src={icon} alt='icon' /> : null}
         </button>
