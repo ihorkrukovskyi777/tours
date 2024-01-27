b8f5ab9890ae42420c1aeefd47d52da40d78cb3e
ynherb <ynherb299@gmail.com>
2024-01-27 02:12:51 +0200

    update modals

--- src/entities/calendar/calendar-provider.js
+++ src/entities/calendar/calendar-provider.js
@@ -1,7 +1,7 @@
 "use client";
-import { createContext, memo } from "react";
-import { StoreCalendar } from "@/entities/calendar/store/store-calendar";
-import { StorePhone } from "@/entities/calendar/store/store-phone";
+import {createContext, memo} from "react";
+import {StoreCalendar} from "@/entities/calendar/store/store-calendar";
+import {StorePhone} from "@/entities/calendar/store/store-phone";
 import Faqs from "@/shared/ui/faqs/faqs";
 import dynamic from "next/dynamic";
 import Loader from "@/shared/ui/loaders/default-loader";
@@ -9,56 +9,53 @@ import Loader from "@/shared/ui/loaders/default-loader";
 import "@/entities/calendar/ui/main/style.css";
 
 const Main = dynamic(() => import("@/entities/calendar/ui/main/main"), {
-  ssr: true,
-  loading: () => (
-    <div
-      className="calendar_wrap"
-      style={{ position: "relative", minHeight: "300px" }}
-    >
-      <Loader style={{ backgroundColor: "inherit" }} />
-    </div>
-  ),
+    ssr: false,
+    loading: () => (
+        <div
+            className="calendar_wrap"
+            style={{position: "relative", minHeight: "300px"}}
+        >
+            <Loader style={{backgroundColor: "inherit"}}/>
+        </div>
+    ),
 });
 
 export const StoreCalendarContext = createContext(null);
 
 export default memo(function CalendarProvider({
-  locale,
-  type,
-  id,
-  activeLanguage,
-  questions,
-}) {
-  let findLocale = activeLanguage?.find((item) => item.code === locale);
-  if (!findLocale) {
-    [findLocale] = activeLanguage;
-  }
-  if (!findLocale) {
-    return null;
-  }
-  return (
-    <StoreCalendarContext.Provider
-      value={{
-        storeCalendar: new StoreCalendar(
-          findLocale.code,
-          type,
-          id,
-          activeLanguage
-        ),
-        storePhone: new StorePhone(findLocale.code),
-      }}
-    >
-      <section id="tour_calendar_section" className="tour_calendar">
-        <div className="container">
-          <div className="wrapper">
-            <Main siteLocale={locale} />
-            <Faqs
-              style={{ paddingRight: 0, paddingLeft: 0 }}
-              questions={questions}
-            />
-          </div>
-        </div>
-      </section>
-    </StoreCalendarContext.Provider>
-  );
+                                                  locale,
+                                                  type,
+                                                  id,
+                                                  activeLanguage,
+                                                  children,
+                                              }) {
+    let findLocale = activeLanguage?.find((item) => item.code === locale);
+    if (!findLocale) {
+        [findLocale] = activeLanguage;
+    }
+    if (!findLocale) {
+        return null;
+    }
+    return (
+        <StoreCalendarContext.Provider
+            value={{
+                storeCalendar: new StoreCalendar(
+                    findLocale.code,
+                    type,
+                    id,
+                    activeLanguage
+                ),
+                storePhone: new StorePhone(findLocale.code),
+            }}
+        >
+            <section id="tour_calendar_section" className="tour_calendar">
+                <div className="container">
+                    <div className="wrapper">
+                        <Main siteLocale={locale}/>
+                        {children}
+                    </div>
+                </div>
+            </section>
+        </StoreCalendarContext.Provider>
+    );
 });
--- src/entities/calendar/ssr-calendar.js
+++ src/entities/calendar/ssr-calendar.js
@@ -1,7 +1,8 @@
 import CalendarProvider from "@/entities/calendar/calendar-provider";
 import {getActiveLang, getFaqBlock} from "@/entities/api";
+import Faqs from "@/shared/ui/faqs/faqs";
 
-export default async function SsrCalendar({locale, type, id, showFaq = true, title }) {
+export default async function SsrCalendar({locale, type, id, showFaq = true, title}) {
 
     const [questions, activeLanguage] = await Promise.all([
         showFaq ? getFaqBlock(id, locale) : Promise.resolve(),
@@ -9,5 +10,20 @@ export default async function SsrCalendar({locale, type, id, showFaq = true, tit
     ])
 
 
-    return <CalendarProvider title={title} locale={locale} type={type} id={id} activeLanguage={activeLanguage} questions={questions} showFaq={showFaq}/>
+    return (
+        <CalendarProvider
+            title={title}
+            locale={locale}
+            type={type}
+            id={id}
+            activeLanguage={activeLanguage}
+            questions={questions}
+            showFaq={showFaq}
+        >
+            <Faqs
+                style={{paddingRight: 0, paddingLeft: 0}}
+                questions={questions}
+            />
+        </CalendarProvider>
+    )
 }
