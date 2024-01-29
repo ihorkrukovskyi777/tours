022c819e3552f0fa13fcd8fae62fa7d26cd30a18
ynherb <ynherb299@gmail.com>
2024-01-29 12:52:54 +0200

    update modals

--- src/widgets/highlights/index.jsx
+++ src/widgets/highlights/index.jsx
@@ -1,12 +1,5 @@
-'use client';
-import IcloudImage from "@/shared/ui/icloud-image";
 import {getHighlightsImages} from "@/entities/api";
 import {createTranslation } from "@/i18n/server";
-import {Navigation, Pagination} from 'swiper/modules';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import './style.css';
 
 export default async function Highlights({id}) {
@@ -17,34 +10,30 @@ export default async function Highlights({id}) {
             <div className="container">
                 <div className="wrapper">
                     <h2 className="title">{t('Highlights of your trip!')}</h2>
-                    <Swiper
-                        // install Swiper modules
-                        modules={[Navigation, Pagination]}
-                        spaceBetween={10}
-                       
-                        slidesPerView={3}
-                        pagination={{ clickable: true }}
-                        breakpoints={{
-                            220: {
-                                slidesPerView: 1.17,
-                            },
-                            768: {
-                                slidesPerView: 3,
-                            },
-                            }}
-                    >   
-                        {images.map((item) => {
-                            return (
-                                <SwiperSlide key={item.src}>
-                                    <IcloudImage width={400} height={250} key={item.src} src={item.src} alt={item.alt} size="500x500" />
-                               </SwiperSlide>
-                            )
-                        })}
-                    </Swiper>
+                    {/*<Swiper*/}
+                    {/*    // install Swiper modules*/}
+                    {/*    modules={[Navigation, Pagination]}*/}
+                    {/*    spaceBetween={10}*/}
 
-
-
-
+                    {/*    slidesPerView={3}*/}
+                    {/*    pagination={{ clickable: true }}*/}
+                    {/*    breakpoints={{*/}
+                    {/*        220: {*/}
+                    {/*            slidesPerView: 1.17,*/}
+                    {/*        },*/}
+                    {/*        768: {*/}
+                    {/*            slidesPerView: 3,*/}
+                    {/*        },*/}
+                    {/*        }}*/}
+                    {/*>*/}
+                    {/*    {images?.map((item) => {*/}
+                    {/*        return (*/}
+                    {/*            <SwiperSlide key={item.src}>*/}
+                    {/*                <IcloudImage width={400} height={250} key={item.src} src={item.src} alt={item.alt} size="500x500" />*/}
+                    {/*           </SwiperSlide>*/}
+                    {/*        )*/}
+                    {/*    })}*/}
+                    {/*</Swiper>*/}
                 </div>
             </div>
 
