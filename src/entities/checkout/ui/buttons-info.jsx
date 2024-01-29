d531343930c66e769ec7f2c98114b177cbc46cbd
Bellmere <therise00@gmail.com>
2024-01-29 22:24:56 +0200

    update checkout Civitatis and buttons

--- src/entities/checkout/ui/buttons-info.jsx
+++ src/entities/checkout/ui/buttons-info.jsx
@@ -1,15 +1,31 @@
+"use client";
+import Button from "@/shared/ui/selectors/button/button";
 
-'use client';
-import Button from "@/shared/ui/selectors/button/button"
-
-
-export default function ButtonsInfo({isOpened , isOpenedModal}) {
-    return (
-        <div className="btn_wrap" >
-            <Button onClick={isOpened}>Edit Booking</Button>
-            <Button customClass="gray" onClick={isOpenedModal}>Contact Your Guide</Button>
-            <a className="button_custom gray" href="http://dev.oneporttest.com/cancel-book/?cancelCode=d619e1e3c3ce2d355c07058d30936f07df227c2a">Cancel Booking</a>
-        </div>
-    )
-  }
-
+export default function ButtonsInfo({
+  isOpened,
+  isOpenedModal,
+  btnController,
+}) {
+  const { showEdit, showCancel, showContact } = btnController;
+  console.log(btnController);
+  return (
+    <div className="btn_wrap">
+      {(showEdit || showCancel) && (
+        <Button onClick={isOpened}>Edit Booking</Button>
+      )}
+      {!showContact && (
+        <Button customClass="gray" onClick={isOpenedModal}>
+          Contact Your Guide
+        </Button>
+      )}
+      {showCancel && (
+        <a
+          className="button_custom gray"
+          href="http://dev.oneporttest.com/cancel-book/?cancelCode=d619e1e3c3ce2d355c07058d30936f07df227c2a"
+        >
+          Cancel Booking
+        </a>
+      )}
+    </div>
+  );
+}
