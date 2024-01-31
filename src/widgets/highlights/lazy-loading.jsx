204694d786335e8c91765ad83ebfea31aaa5baf5
ynherb <ynherb299@gmail.com>
2024-01-31 02:22:52 +0200

    add bookings

--- src/widgets/highlights/lazy-loading.jsx
+++ src/widgets/highlights/lazy-loading.jsx
@@ -2,18 +2,17 @@
 import {useRef} from "react";
 import useOnScreen from "@/shared/hooks/useOnScreen";
 import dynamic from "next/dynamic";
-const SwiperGuides = dynamic(
-    () => import("@/shared/ui/guides/swiper/swiper"),
+const SwiperHighlights = dynamic(
+    () => import("@/widgets/highlights/slider"),
     { ssr: false }
 )
 
 export default function LazyHighlights({images}) {
     const ref= useRef(null)
     const isVisible = useOnScreen(ref, (ref) => ref?.closest('section') ?? ref)
-
     return (
         <div ref={ref}>
-            {isVisible ? <SwiperGuides images={images}/> : null}
+            {isVisible ? <SwiperHighlights images={images}/> : null}
         </div>
     )
 }
