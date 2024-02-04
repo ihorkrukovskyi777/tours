df51266591555ed9352428be4f737ed40ed60da5
ynherb <ynherb299@gmail.com>
2024-02-04 02:57:16 +0200

    add sitemap

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     experimental: { optimizeCss: true },
-
+    compress: false,
     swcMinify: true,
     // compiler: {
     //     removeConsole: {
