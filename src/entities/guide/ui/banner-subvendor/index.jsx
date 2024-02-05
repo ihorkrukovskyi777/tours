50650f861e11b4c764a0eab2e9ac5b97df66bbe7
Igor <ihor.krukovskyi@gmail.com>
2024-02-05 16:39:19 +0200

    update

--- src/entities/guide/ui/banner-subvendor/index.jsx
+++ src/entities/guide/ui/banner-subvendor/index.jsx
@@ -1,19 +1,19 @@
 import IcloudImage from "@/shared/ui/icloud-image";
-import styles from './style.module.css';
+import './style.css';
 
 export default function BannerSubVendor({attachment = null, name, children, avatar, isMobile = false}) {
     const [width, height] = isMobile ? [430, 430] : [1200, 1200]
     return (
         <section className="banner_subvendor">
-            <IcloudImage className={styles.banner_bg} src={attachment?.src} alt={attachment?.alt} width={width}
+            <IcloudImage className="banner_bg" src={attachment?.src} alt={attachment?.alt} width={width}
                          height={height}/>
             <div className="container">
-                <div className={styles.wrapper}>
-                    <div className={styles.img_box}>
+                <div className="wrapper">
+                    <div className="img_box">
                         <IcloudImage src={avatar} width={240} height={240}></IcloudImage>
                     </div>
                     <div>
-                        <h1 className={styles.title}>{name}</h1>
+                        <h1 className="title">{name}</h1>
                         {children}
                     </div>
                 </div>
