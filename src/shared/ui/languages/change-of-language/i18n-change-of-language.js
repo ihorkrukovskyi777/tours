2f7c3a9a3a6d213460f4af7553d2d1d9c2dbc5d6
ynherb <ynherb299@gmail.com>
2024-02-07 03:11:44 +0200

    fixed

--- src/shared/ui/languages/change-of-language/i18n-change-of-language.js
+++ src/shared/ui/languages/change-of-language/i18n-change-of-language.js
@@ -6,7 +6,7 @@ export default async function I18nChangeOfLanguage({locale, title, ...props}) {
     return (
         <ChangeOfLanguage
             {...props}
-            title={i18n.t(title)}
+            title={title}
             i18n={{
                 load_more: i18n.t('Load More'),
                 free_tour_tour_language: i18n.t('Free Tours in Your Language')
