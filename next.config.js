8d4bd142d39cf4fa5e59f0744b2381f67baf38a3
ynherb <ynherb299@gmail.com>
2024-02-13 00:48:37 +0200

    add redirect

--- next.config.js
+++ next.config.js
@@ -17,6 +17,17 @@ const nextConfig = {
             },
         ],
     },
+    headers: () => [
+        {
+            source: '/',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'public, max-age=9999999999, must-revalidate',
+                },
+            ],
+        },
+    ],
 }
 
 module.exports = nextConfig
