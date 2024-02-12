422d5e1cf5ec49592e949ab0f86642549de714f6
ynherb <ynherb299@gmail.com>
2024-02-13 01:11:44 +0200

    add redirect

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -16,14 +16,16 @@ export default async function LocaleLayout({children, params}) {
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
-                window.onpageshow = function(e) {
-                
-                    if (e.persisted) {
-                
-                        alert("Page shown");
+                window.addEventListener( "pageshow", function ( event ) {
+                      var historyTraversal = event.persisted || 
+                                             ( typeof window.performance != "undefined" && 
+                                                  window.performance.navigation.type === 2 );
+                      if ( historyTraversal ) {
+                      alert(2);
+                        // Handle page restore.
                         window.location.reload();
-                    }
-                };
+                      }
+                    });
 
             `,
                 }}
