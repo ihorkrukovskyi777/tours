0c5b0d2498abe57454899d6ae5044898e5040894
ynherb <ynherb299@gmail.com>
2024-03-11 16:09:42 +0200

    fixed posts

--- src/entities/calendar/ssr-calendar.js
+++ src/entities/calendar/ssr-calendar.js
@@ -1,12 +1,8 @@
+import CalendarProvider from "@/entities/calendar/calendar-provider";
 import {getActiveLang, getFaqBlock} from "@/entities/api";
 import Faqs from "@/shared/ui/faqs/faqs";
 import useDefaultI18n from "@/i18n/hooks/useDefaultI18n";
 import useGenitiveI18n from "@/i18n/hooks/useGenitiveI18n";
-import dynamic from "next/dynamic";
-const CalendarProvider = dynamic(
-    () => import("@/entities/calendar/calendar-provider"),
-    {ssr: false, loading: () => <div style={{minHeight: '500px'}}></div>}
-)
 
 
 export default async function SsrCalendar({locale, type, id, showFaq = true, title, nameDayWeek = true }) {
