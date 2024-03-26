576cb5ceb4265c6f15870e323cd96990f625d242
ynherb <ynherb299@gmail.com>
2024-03-26 14:06:03 +0200

    add insert script

--- src/widgets/insert-code/client/client-insert-code.jsx
+++ src/widgets/insert-code/client/client-insert-code.jsx
@@ -5,7 +5,7 @@ export default memo(function ClientInsertCode({ scripts, children, scriptInner }
 
     return (
         <div className="insert-a-code container">
-            <Script dangerouslySetInnerHTML={{__html: scriptInner}} id="inser-code-script"></Script>
+            <Script dangerouslySetInnerHTML={{__html: scriptInner}} id={`${Math.random()}`}></Script>
             {scripts.map((src, index) =>  <Script id={src} async={true} defer={true} key={index} src={src}></Script>)}
             {children}
         </div>
