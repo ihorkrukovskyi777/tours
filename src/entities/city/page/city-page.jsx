1ae62b94e9083039c7aac3ddced9f23be52bf204
ynherb <ynherb299@gmail.com>
2024-03-26 02:52:45 +0200

    update schema

--- src/entities/city/page/city-page.jsx
+++ src/entities/city/page/city-page.jsx
@@ -1,15 +1,15 @@
 import {Suspense} from "react";
 import BannerCity from "@/entities/city/ui/banner-city";
-// import SsrCalendar from "@/entities/calendar/ssr-calendar";
-import MostPopularTours from "@/entities/city/ui/most-popular-tours";
-import LatestReviews from "@/widgets/latest-reviews";
-import Highlights from "@/widgets/highlights";
-import TextBlocks from "@/widgets/text-blocks";
-import Guides from "@/shared/ui/guides";
-import MostPopularCity from "@/entities/city/ui/most-popular-city";
-import I18nChangeOfLanguage from "@/shared/ui/languages/change-of-language/i18n-change-of-language";
-import Breadcrumbs from "@/shared/ui/breadcrumbs";
-import Footer from "@/shared/ui/layouts/footer/footer";
+import SsrCalendar from "@/entities/calendar/ssr-calendar";
+// import MostPopularTours from "@/entities/city/ui/most-popular-tours";
+// import LatestReviews from "@/widgets/latest-reviews";
+// import Highlights from "@/widgets/highlights";
+// import TextBlocks from "@/widgets/text-blocks";
+// import Guides from "@/shared/ui/guides";
+// import MostPopularCity from "@/entities/city/ui/most-popular-city";
+// import I18nChangeOfLanguage from "@/shared/ui/languages/change-of-language/i18n-change-of-language";
+// import Breadcrumbs from "@/shared/ui/breadcrumbs";
+// import Footer from "@/shared/ui/layouts/footer/footer";
 import PlaceSchema from "@/shared/schema/place";
 import ProductSchema from "@/shared/schema/product";
 import EventsSchema from "@/shared/schema/events";
@@ -32,7 +32,7 @@ export default async function CityPage({locale, title, id, languages, slug, isMo
             <PlaceSchema id={id} locale={locale}/>
             <ProductSchema id={id} locale={locale}/>
             <Suspense fallback={''}>
-                {/*<SsrCalendar locale={locale} type="city" id={id} title={i18n.tReplace('%s Tour Calendar', title)}/>*/}
+                <SsrCalendar locale={locale} type="city" id={id} title={i18n.tReplace('%s Tour Calendar', title)}/>
                 {/*<MostPopularTours id={id} locale={locale} slug={slug} title={title} size={'small'}/>*/}
                 {/*<LatestReviews id={id} locale={locale}/>*/}
                 {/*<Highlights id={id} locale={locale}/>*/}
