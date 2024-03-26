ca8206ac4b60d295df15babe3c675f759b55f712
ynherb <ynherb299@gmail.com>
2024-03-26 02:58:00 +0200

    update schema

--- src/entities/calendar/calendar-provider.js
+++ src/entities/calendar/calendar-provider.js
@@ -2,8 +2,14 @@
 import {createContext, memo, useEffect} from "react";
 import {StoreCalendar} from "@/entities/calendar/store/store-calendar";
 import {StorePhone} from "@/entities/calendar/store/store-phone";
-import Main from "@/entities/calendar/ui/main/main";
+const Main = dynamic(
+    () => import("@/entities/calendar/ui/main/main"),
+    {
+        ssr: false,
+    }
+)
 import "@/entities/calendar/ui/main/style.css";
+import dynamic from "next/dynamic";
 
 export const StoreCalendarContext = createContext(null);
 
