60f90c0095671897db6387350c3c142278732b46
ynherb <ynherb299@gmail.com>
2024-04-09 17:02:15 +0300

    update

--- src/entities/city/ui/partner-tours/index.js
+++ src/entities/city/ui/partner-tours/index.js
@@ -3,12 +3,13 @@ import PartnerRowTours from "@/shared/ui/card-components/partner-row-tours/partn
 import useDefaultI18n from "@/i18n/hooks/useDefaultI18n";
 
 import { picketPartnerTours } from "@/entities/api";
+import {log} from "util";
 
 export default async function PartnerTours({ id, locale, size = "small" }) {
   let data = await picketPartnerTours(id, locale);
   const i18n = await useDefaultI18n(locale);
 
-  if (!Array.isArray(data.partnerTours)) {
+  if (!Array.isArray(data.partnerTours) || data.partnerTours.length === 0) {
     return null;
   }
 
