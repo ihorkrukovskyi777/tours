4ec5528d5f7732f4e08af751408017973b97e150
ynherb <ynherb299@gmail.com>
2024-04-11 10:27:24 +0300

    fixed mobx clone

--- src/entities/calendar/store/store-calendar.js
+++ src/entities/calendar/store/store-calendar.js
@@ -1,9 +1,11 @@
-import {makeAutoObservable, onBecomeObserved} from "mobx"
+import {makeAutoObservable, configure} from "mobx"
 import {StoreLoading} from "@/entities/calendar/store/store-loading";
 import {StoreTourLogic} from "@/entities/calendar/store/store-tour-logic";
 import {StoreModalBooking} from "@/entities/calendar/store/store-modal-booking";
 import {StoreModalCalendar} from "@/entities/calendar/store/store-modal-calendar";
+configure( { isolateGlobalState: true });
 export class StoreCalendar {
+
     constructor(locale, type, id, activeLanguage, title, localeError) {
         this.type = type;
         this.id = id;
