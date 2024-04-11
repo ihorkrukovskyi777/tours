f217a995e0d13994ef2c418acdad75cac9dddd01
ynherb <ynherb299@gmail.com>
2024-04-11 11:14:50 +0300

    fixed mobx clone

--- src/widgets/insert-code/insert-partner-code.jsx
+++ src/widgets/insert-code/insert-partner-code.jsx
@@ -30,9 +30,10 @@ export default async function InsertPartnerCode({
         {data?.title?.trim() && (
             <ViewQuote title={data.title} description={data.description} />
         )}
-      <ClientInsertCode scripts={scripts} scriptInner={scriptInner}>
-        {html}
-      </ClientInsertCode>
+        <iframe src={`${process.env.NEXT_PUBLIC_NEST_API}/api/v1/tour/endpoint`} frameBorder="0"></iframe>
+      {/*<ClientInsertCode scripts={scripts} scriptInner={scriptInner}>*/}
+      {/*  {html}*/}
+      {/*</ClientInsertCode>*/}
     </>
   );
 }
