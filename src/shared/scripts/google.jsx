2641dee2e9ce1f1690c67f3981411c0b50c59f23
Igor <ihor.krukovskyi@gmail.com>
2024-05-02 18:03:55 +0300

    update

--- src/shared/scripts/google.jsx
+++ src/shared/scripts/google.jsx
@@ -79,27 +79,15 @@ export default function GoogleScript() {
                         const arrTranslate = {'en-US': 'Configure' , 'es-ES': 'Configurar'};
                         const lang = document.querySelector('html').getAttribute('lang');
                         const title = arrTranslate[lang]  ? arrTranslate[lang] : arrTranslate['en-US'];
-                        /*const loadKlaroJS =  setInterval(function () {
-                            if(document.querySelector('.klaro') !== null) {
+                        const loadKlaroJS =  setInterval(function () {
+                            if(document.querySelector('.klaro') !== null && document.querySelector('.cm-footer-buttons') !== null) {
                                 document.querySelector('.cm-footer-buttons').insertAdjacentHTML("afterbegin", `<button class='cm-btn cm-btn-success cm-btn-info toggle-body'>${title}</button>`);
                                 document.querySelector('.toggle-body').addEventListener('click', function () {
                                     document.querySelector('.klaro .cm-body').classList.toggle('open');
                                 })
                                 clearInterval(loadKlaroJS);
                             }
-                        } , 110)*/
-
-                        window.requestAnimationFrame(() => {
-                            if(document.querySelector('#klaro') !== null && document.querySelector('.cm-footer-buttons') !== null) {
-                                document.querySelector('.cm-footer-buttons').insertAdjacentHTML("afterbegin", `<button class='cm-btn cm-btn-success cm-btn-info toggle-body'>${title}</button>`);
-                                document.querySelector('.toggle-body').addEventListener('click', function () {
-                                    document.querySelector('.klaro .cm-body').classList.toggle('open');
-                                })
-
-                            }
-                        })
-
-
+                        } , 120)
 
                     } catch (error) {
                         console.log(error)
