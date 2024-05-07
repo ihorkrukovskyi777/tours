ca04be17c8a4701773ababdc49a79b2bb1154259
ynherb <ynherb299@gmail.com>
2024-05-07 15:57:56 +0300

    add text

--- src/entities/city/ui/banner-city/index.jsx
+++ src/entities/city/ui/banner-city/index.jsx
@@ -22,7 +22,7 @@ export default async function BannerCity({id, locale, size, isMobile = false, pa
             size={size}
             bottomView={<Reviews rating={rating} count_reviews={reviews} title={i18n.t('Reviews')}/>}
         >
-            <p className="pick_text">{i18n.t('Pick a Date!')}</p>
+            <p className="pick_text pick_text--md">{i18n.t('See the best Free Tours of {city} offered by local tour guides')?.replace('{city}', title).replaceAll('<br>', '').replaceAll('</br>', '')}</p>
             <div className="flex-row">
                 <BannerButtons i18n={{today: i18n.t('Today'), tomorrow: i18n.t('Tomorrow')}} />
             </div>
