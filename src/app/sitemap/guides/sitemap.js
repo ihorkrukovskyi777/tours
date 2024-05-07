64e399d3071b5a5084e12f153f134a53797891ec
ynherb <ynherb299@gmail.com>
2024-05-07 09:49:05 +0300

    update schema

--- src/app/sitemap/guides/sitemap.js
+++ src/app/sitemap/guides/sitemap.js
@@ -2,11 +2,11 @@ export default async function sitemap() {
     let siteMaps = await fetch(`${process.env.NEXT_PUBLIC_NEST_API}/api/v1/seo/sitemap/sub-vendors`, {next: { revalidate: 60 * 60, tags: ['seo'] }});
     siteMaps = await siteMaps.json();
     const getSlug = (page) => {
-        return page.locale === 'en' ? `${page.slug}` : `${page.locale}/${page.slug}`
+        return page.locale === 'en' ? `` : `${page.locale}`
     }
 
     return siteMaps.map(page => ({
-        url: `${process.env.NEXT_PUBLIC_CANONICAL_DOMAIN}/guide/${getSlug(page)}`,
+        url: `${process.env.NEXT_PUBLIC_CANONICAL_DOMAIN}${getSlug(page)}/guide/${page.slug}`,
         lastModified: page.lastModified,
     }))
 }
