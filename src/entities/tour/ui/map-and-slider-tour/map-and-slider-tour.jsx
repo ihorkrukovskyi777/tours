4a0035b999847a959869f51e906246f11b988011
ynherb <ynherb299@gmail.com>
2024-05-08 11:04:58 +0300

    remove places

--- src/entities/tour/ui/map-and-slider-tour/map-and-slider-tour.jsx
+++ src/entities/tour/ui/map-and-slider-tour/map-and-slider-tour.jsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
 import {placesMarkers} from "@/entities/api";
 import useDefaultI18n from "@/i18n/hooks/useDefaultI18n";
-import PlacesFixedSeo from "@/shared/ui/seo-fixed/places";
 
 const ProviderMap = dynamic(
     () => import("@/widgets/map-and-slider/provider"),
@@ -15,9 +14,6 @@ export default async function MapAndSliderTour({children, hideBottom, locale, id
             <ProviderMap places={places} hideBottom={hideBottom} locale={locale} id={id} i18n={i18n.getMapSliders()}>
                 {children}
             </ProviderMap>
-            <ul style={{display: "none"}} id="tour_places">
-                {places?.map(place => <PlacesFixedSeo key={place.id} title={place.title} tickets={place.tickets}  attachment={place.attachment} i18n={i18n.getMapSliders()}/>)}
-            </ul>
         </>
     )
 }
