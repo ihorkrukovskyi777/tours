d466de03ba244748c54da25c0d831deff93d2fa3
ynherb <ynherb299@gmail.com>
2024-05-08 10:10:15 +0300

    add places tours

--- src/shared/ui/card-components/card-text/index.jsx
+++ src/shared/ui/card-components/card-text/index.jsx
@@ -4,7 +4,7 @@ export default function CardText({children , title}) {
     return (
         <div className={styles.block_wrap}>
             <div className={styles.text_wrapper}>
-                <h2 className={styles.block_title}>{title}</h2>
+                <h3 className={styles.block_title}>{title}</h3>
                 <div className={styles.block_text}>{children}</div>
             </div>
         </div>  
