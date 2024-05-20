a336330cfd39aae32d6e0cc1398891f991fb56c5
ynherb <ynherb299@gmail.com>
2024-05-20 15:20:21 +0300

    add pagination sitemap

--- src/app/sitemap/pages/sitemap.js
+++ src/app/sitemap/pages/sitemap.js
@@ -1,6 +1,25 @@
-export default async function sitemap() {
-    let siteMaps = await fetch(`${process.env.NEXT_PUBLIC_NEST_API}/api/v1/seo/sitemap/pages`, {next: { revalidate: 60 * 60, tags: ['seo'] }});
-    siteMaps = await siteMaps.json();
+import {generatePages, LIMIT} from "@/shared/constants/sitemap";
+import {fetchTours} from "@/app/sitemap/tours/sitemap";
+
+export async function fetchPages() {
+    let siteMaps = await fetch(`${process.env.NEXT_PUBLIC_NEST_API}/api/v1/seo/sitemap/sub-vendors`, {next: { revalidate: 60 * 60, tags: ['seo'] }});
+    return siteMaps.json();
+}
+
+export async function generateSitemaps() {
+    try {
+        const total = await fetchPages();
+        const sitemaps = generatePages(total.length);
+        return sitemaps;
+    } catch (error) {
+        console.error("Error generating sitemaps: ", error);
+        return [];
+    }
+}
+
+export default async function sitemap({ id }) {
+    let siteMaps = await fetchPages();
+
 
     const getSlug = (page) => {
         if(page.locale === 'en' && page.slug === '') {
@@ -8,7 +27,7 @@ export default async function sitemap() {
         }
         return page.locale === 'en' ? `/${page.slug}` : `/${page.locale}/${page.slug}`
     }
-    return siteMaps.map(page => ({
+    return siteMaps.slice(id * LIMIT, LIMIT * (id + 1)).map(page => ({
         url: `${process.env.NEXT_PUBLIC_CANONICAL_DOMAIN}${getSlug(page)}`,
         lastModified: page.lastModified,
     }))
--- src/app/sitemap/posts/sitemap.js
+++ src/app/sitemap/posts/sitemap.js
@@ -1,9 +1,27 @@
-export default async function sitemap() {
+import {generatePages, LIMIT} from "@/shared/constants/sitemap";
+import {fetchTours} from "@/app/sitemap/tours/sitemap";
+
+export async function fetchPosts() {
     let siteMaps = await fetch(`${process.env.NEXT_PUBLIC_NEST_API}/api/v1/seo/sitemap/posts`, {next: { revalidate: 60 * 60, tags: ['seo'] }});
-    siteMaps = await siteMaps.json();
+    return siteMaps.json();
+}
+
+export async function generateSitemaps() {
+    try {
+        const total = await fetchPosts();
+        const sitemaps = generatePages(total.length);
+        return sitemaps;
+    } catch (error) {
+        console.error("Error generating sitemaps: ", error);
+        return [];
+    }
+}
+export default async function sitemap({ id }) {
+    let siteMaps = await fetchPosts();
+
 
     const getSlug = (page) => page.locale === 'en' ? `${page.slug}` : `${page.locale}/${page.slug}`
-    return siteMaps.map(page => ({
+    return siteMaps.slice(id * LIMIT, LIMIT * (id + 1)).map(page => ({
         url: `${process.env.NEXT_PUBLIC_CANONICAL_DOMAIN}/${getSlug(page)}`,
         lastModified: page.lastModified,
     }))
