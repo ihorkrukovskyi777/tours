ae9acd29b01c3e4d1e4f64b284a105e520cf95b1
Ihor <ihor.krukovskyi@gmail.com>
2024-12-05 18:06:21 +0200

    update

--- src/shared/ui/card-components/card-guide/index.jsx
+++ src/shared/ui/card-components/card-guide/index.jsx
@@ -5,27 +5,22 @@ import FlagsComponents from "@/shared/ui/flags";
 import imagePlay from '@/assets/images/svg/play.svg';
 import styles from './style.module.css'
 import {useRef, useState} from "react";
+import { Stream } from "@cloudflare/stream-react";
 import Image from "next/image";
-export default function CardGuide({children , avatar , url , bottomView = [] , videoURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4'}) {
+
+export default function CardGuide({children , avatar , url , bottomView = [] , videoURL = ''}) {
     const router = useRouter();
 
+    const [isPlay, setIsPlay] = useState(false);
+
     const videoRef = useRef(null);
-    const  [statusPlay , setStatusPlay] = useState(false);
-
-    const handlePlay = () => {
+    const playVideo = () => {
         videoRef.current.play();
-        setStatusPlay(true);
     };
-
-    const handlePause = () => {
+    const pauseVideo = () => {
         videoRef.current.pause();
-        setStatusPlay(false);
     };
-
-    console.log(statusPlay);
-
     return (
-
 
         <div
             onClick={() => {
@@ -39,34 +34,30 @@ export default function CardGuide({children , avatar , url , bottomView = [] , v
 
             <div className="img_box">
                 {videoURL &&
-                    <div className="video_block">
-                        <video
-                            ref={videoRef}
-                            controls={false}
-                        >
-                            <source src={videoURL} type="video/mp4" />
-                            Your browser does not support the video tag.
-                        </video>
-                        {!statusPlay &&
-                            <button onClick={handlePlay} className="play">
-                                <Image
-                                    src={imagePlay}
-                                    alt="play"
-                                    width={48}
-                                    height={48}
-                                    style={{ fill: "red" }}
-                                />
-                            </button>
-                        }
-                        {statusPlay && <button onClick={handlePause} className="pause"></button> }
+                    <div>
+
+                        <Stream
+                            streamRef={videoRef}
+                            src={videoURL}
+                        />
+                        <button onClick={playVideo} type="button">
+                            Watch
+                        </button>
+                        <button onClick={pauseVideo} type="button">
+                            pause
+                        </button>
+
+
                     </div>
                 }
             </div>
 
-                <div onClick={() => {if(videoURL.length > 0) router.push(url?.toLowerCase())}}>
+                <div onClick={() => {
+                    if (videoURL.length > 0) router.push(url?.toLowerCase())
+                }}>
                     {children}
                     <div className='flags_wrap'>
-                        {bottomView.map((item , index) => (
+                        {bottomView.map((item, index) => (
                             <div key={index} className='flag'>
                                 <FlagsComponents locale={item} alt={`flag`}  className='country-box-select'/>
                             </div>
