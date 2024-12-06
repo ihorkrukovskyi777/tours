892b8864e1eb7ec0d40269ec60882c34347b88c4
ynherb <ynherb299@gmail.com>
2024-12-06 21:12:38 +0200

    hide text block if empty

--- src/shared/ui/banner/index.jsx
+++ src/shared/ui/banner/index.jsx
@@ -1,20 +1,19 @@
-import IcloudImage from "@/shared/ui/icloud-image";
 import './style.css';
 export default function Banner({headMobile= null, attachment = null, title, children, bottomView = null, size , nameBanner='', isMobile = false}) {
     const [width, height] = isMobile ? [430, 430] : [1900, 1200]
+
+    const image = `${process.env.NEXT_PUBLIC_CLOUD_IMAGE}/${attachment?.src}/${isMobile ? '500x500' : 'public'}`;
     return (
         <section className={`banner ${nameBanner} ${headMobile ? 'head_mobile' : ''}`} style={{zIndex: 998, position: 'relative'}}>
             {headMobile ?
                 <div className="banner_head_mobile">{headMobile}</div>
             : null}
             <div className={size}>
-                 <IcloudImage
-                    priority={true}
+                 <img
                     className="banner_bg"
                     width={width}
                     height={height}
-                    size={ isMobile ? '500x500' : 'public'}
-                    src={attachment?.src}
+                    src={image}
                     alt={attachment?.alt}
                 />
                 <div className="container">
