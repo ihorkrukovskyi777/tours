b214a8ebc532d9e6e5269a7bea223ec0cf2279a8
ihorkrukovskyi777 <ihor.krukovskyi@gmail.com>
2025-01-16 12:45:09 +0200

    update

---
+++ src/shared/ui/card-components/congratulation-card/congratulation-card.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import './style.css';
+
+interface Props {
+    title: string,
+    url: string,
+    attachment?: string,
+    sale?: string,
+}
+
+
+const CongratulationCard = ({title, attachment, url, sale}: Props) => {
+    return (
+        <div className="congratulation-card">
+            <Link href={url}>
+                {attachment &&
+                    <img
+                        loading="lazy"
+                        className="img"
+                        key={attachment.src}
+                        width={518}
+                        height={250}
+                        src={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE}/${attachment.src}/${size}`}
+                        alt={attachment.alt ?? 'trip'}
+                    />
+                }
+                <div className="box">
+                    <h2>{title}</h2>
+                </div>
+                <div className="bottom_info">
+                    {sale && <div className="sale">{sale}</div>}
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default CongratulationCard;
+
---
+++ src/widgets/congratulations/index.tsx
@@ -0,0 +1,36 @@
+
+import Link from "next/link";
+import Button from "@/shared/ui/selectors/button/button";
+import {ReactNode} from "react";
+
+import './style.css';
+
+interface Props {
+    children?: ReactNode,
+    title: string,
+    subTitle?: string,
+    locale?: string,
+    id?: number,
+
+}
+
+const Congratulations = ({title , subTitle , children}: Props) => {
+    return (
+        <section className="congratulations">
+            <div className="container">
+                <div className="wrapper">
+                    <h1>{title}</h1>
+                    <p className="sub_title">{subTitle}</p>
+                    <div className="cards">
+                        {children}
+                    </div>
+                    <Link className="see-all" href={'/'}>See all paid tours</Link>
+                    <Button customClass={'create_account'}>Create account and save my credit</Button>
+                    <Button customClass={'decline_credit'}>Decline Credit</Button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Congratulations;
