09e126d06cff114741e4bcff243cb28610fce613
ihorkrukovskyi777 <ihor.krukovskyi@gmail.com>
2025-02-07 15:28:12 +0200

    update

--- src/shared/ui/layouts/footer/footer-menus.jsx
+++ src/shared/ui/layouts/footer/footer-menus.jsx
@@ -1,30 +1,56 @@
 'use client'
-import {useRouter} from "next/navigation";
+import {useRouter , usePathname} from "next/navigation";
 import {getHrefLocale} from "@/i18n/get-href-locale";
+
+
 
 const styles = {
     cursor: 'pointer',
 }
 export default function FooterMenus({ menuItems }) {
-    const router = useRouter()
+    const router = useRouter();
+    const pathname = usePathname();
+    const hideLegalPage = 1
     return (
         <>
-            {menuItems?.map((menu) => (
-                <li
-                    style={styles}
-                    key={menu.slug}
-                    onClick={() => {
-                        if(window?.bugPageRoute) {
-                            window.location.href = getHrefLocale(menu.locale, menu.slug)
-                        } else {
-                            router.push(getHrefLocale(menu.locale, menu.slug))
-                        }
 
-                    }}
-                >
-                    {menu.title || ""}
-                </li>
-            ))}
+            {pathname === '/' &&
+                menuItems?.map((menu) => (
+                    <li
+                        style={styles}
+                        key={menu.slug}
+                        onClick={() => {
+                            if (window?.bugPageRoute) {
+                                window.location.href = getHrefLocale(menu.locale, menu.slug);
+                            } else {
+                                router.push(getHrefLocale(menu.locale, menu.slug));
+                            }
+                        }}
+                    >
+                        {menu.title || ""}
+                    </li>
+                ))
+            }
+
+            {pathname !== '/' &&
+                menuItems?.map((menu, index) =>
+                    index !== hideLegalPage && (
+                        <li
+                            style={styles}
+                            key={menu.slug}
+                            onClick={() => {
+                                if (window?.bugPageRoute) {
+                                    window.location.href = getHrefLocale(menu.locale, menu.slug);
+                                } else {
+                                    router.push(getHrefLocale(menu.locale, menu.slug));
+                                }
+                            }}
+                        >
+                            {menu.title || ""}
+                        </li>
+                    )
+                )
+            }
         </>
     )
 }
