1b27bf5261ec3080b143fede65abeb47ac486616
ynherb <ynherb299@gmail.com>
2025-04-09 17:47:26 +0300

    add not found home page banner

--- src/entities/analytics/types.ts
+++ src/entities/analytics/types.ts
@@ -1,8 +1,24 @@
-
-export type AnalyticsType = 'click_pick_a_date' | 'click_day_list' | 'show_additional_modal'
-    | 'closed_additional_sales' | 'first_booking' | 'additional_booking' | 'show_coupon_modal' | 'send_coupon_email'
- | 'decline_coupon' | 'close_coupon_modal' | 'accepted_the_coupon' | 'booking_confirmation_page' | 'click_see_all_paid_tours' | 'click_paid_tour_card_modal'
-    | 'left_the_see_all_paid_tours' | 'pick_a_date_paid_tour' | 'pick_a_date_paid_tour_full_date' | 'checkout_page'
+export type AnalyticsType =
+    'click_pick_a_date'
+    | 'click_day_list'
+    | 'show_additional_modal'
+    | 'closed_additional_sales'
+    | 'closed_browser_additional_sales'
+    | 'first_booking'
+    | 'additional_booking'
+    | 'show_coupon_modal'
+    | 'send_coupon_email'
+    | 'decline_coupon'
+    | 'close_coupon_modal'
+    | 'close_browser_coupon_modal'
+    | 'accepted_the_coupon'
+    | 'booking_confirmation_page'
+    | 'click_see_all_paid_tours'
+    | 'click_paid_tour_card_modal'
+    | 'left_the_see_all_paid_tours'
+    | 'pick_a_date_paid_tour'
+    | 'pick_a_date_paid_tour_full_date'
+    | 'checkout_page'
 
 
 export interface AnalyticsData {
@@ -15,6 +31,7 @@ export interface AnalyticsData {
 export interface AnalyticsDataLeft extends AnalyticsData {
     redirect_pathname: string
 }
+
 export interface AnalyticsEvent {
     type: AnalyticsType,
 }
