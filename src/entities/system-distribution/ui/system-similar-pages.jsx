18af0d5ed6077692205ae0411b8d4aa8a60cf8db
7fridays <7fridaysdeveloper@gmail.com>
2026-03-10 14:29:29 +0200

    update translate

--- src/entities/system-distribution/ui/system-similar-pages.jsx
+++ src/entities/system-distribution/ui/system-similar-pages.jsx
@@ -1,4 +1,4 @@
-import useDefaultI18n from '@/i18n/hooks/useDefaultI18n';
+import { getTranslations } from 'next-intl/server';
 import { getHrefLocale } from '@/i18n/get-href-locale';
 import { getSystemSimilarPages } from '@/entities/system-distribution/api';
 import Card from '@/shared/ui/card-components/card/card';
@@ -9,15 +9,15 @@ import '@/shared/ui/card-components/row-cities/style.css';
  */
 export default async function SystemSimilarPages({ id, locale }) {
     const similarPages = await getSystemSimilarPages(id, locale);
-    const i18n = await useDefaultI18n(locale);
     if (!Array.isArray(similarPages) || similarPages.length === 0) {
         return null;
     }
 
-    const title = i18n.t('similar_pages');
+    const t = await getTranslations();
+    const title = t('similar_pages');
 
     return (
-        <section className="most_popular_city">
+        <section className="most_popular_city small similar_pages_cards">
             <div className="container">
                 <div className="wrapper">
                     <h2 className="title">{title}</h2>
